06ee0337532a23dc56f8e4722e43913ecd48ba3a
Ouyang Mingjie <106028061+jefferyou@users.noreply.github.com>
2025-12-27 20:40:45 +0800

    Add files via upload

---
+++ VecCity-main/veccity/upstream/region_representation/__init__.py
@@ -0,0 +1,26 @@
+from veccity.upstream.region_representation.Node2Vec import Node2Vec
+from veccity.upstream.region_representation.HDGE import HDGE
+from veccity.upstream.region_representation.ZEMob import ZEMob
+from veccity.upstream.region_representation.MGFN import MGFN
+from veccity.upstream.region_representation.LINE import LINE
+from veccity.upstream.region_representation.MVURE import MVURE
+from veccity.upstream.region_representation.GMEL import GMEL
+from veccity.upstream.region_representation.ReMVC import ReMVC
+from veccity.upstream.region_representation.HREP import HREP
+from veccity.upstream.region_representation.HAFusion import HAFusion
+from veccity.upstream.region_representation.HGI import HGI
+from veccity.upstream.region_representation.ReDCL import ReDCL
+__all__ = [
+    "Node2Vec",
+    "HDGE",
+    "ZEMob",
+    "MGFN",
+    "LINE",
+    "MVURE",
+    "GMEL",
+    "ReMVC",
+    "HREP",
+    "HAFusion",
+    "HGI",
+    "ReDCL"
+]
